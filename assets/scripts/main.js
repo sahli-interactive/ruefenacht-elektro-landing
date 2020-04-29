d9d01531e8915fe055fd01f0a075b3a8558266e7
Emanuel Imhof <hello@m43nu.ch>
2020-04-29 22:25:53 +0200

    bugfix - try to fix the missing tooltip

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,13 @@
 // Vendors
 import './vendor/jquery'
-import 'popper.js'
+import '@popperjs/core'
 import './vendor/bootstrap'
 import './vendor/fontawesome'
 
-$('form :input').focus(function() {
-    $('label[for="' + this.id + '"]').addClass('labelfocus');
-}).blur(function() {
-    $('label[for="' + this.id + '"]').removeClass('labelfocus');
+$('form :input').focus(function () {
+  $('label[for="' + this.id + '"]').addClass('labelfocus');
+}).blur(function () {
+  $('label[for="' + this.id + '"]').removeClass('labelfocus');
 });
 
 $(function () {
